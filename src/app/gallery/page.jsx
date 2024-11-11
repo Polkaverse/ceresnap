c7f01bd2bdea90313f5eb912ac83c83ea69144a3
Polkaverse <pankajchaudhary172@gmail.com>
2024-11-11 11:03:02 +0700

    updated gradient and gitignore

--- src/app/gallery/page.jsx
+++ src/app/gallery/page.jsx
@@ -24,25 +24,51 @@ export default function ListFiles() {
   }, []);
 
   return (
-    <div className="flex min-h-screen bg-white relative isolate px-6 pt-14 lg:px-8">
-      <div className="mx-auto mt-10 mb-auto max-w-lg rounded-lg bg-white p-5 shadow-md">
-        <h2 className="text-lg text-black font-medium mb-5">Gallery Photos</h2>
-        {error && <p className="text-red-500">{error}</p>}
-        <ul className="space-y-3">
-          {files.map((file) => (
-            <li key={file.cid}>
-              <a
-                href={file.url}
-                target="_blank"
-                rel="noopener noreferrer"
-                className="text-blue-600 hover:underline"
-              >
-                View File (CID: {file.cid})
-              </a>
-            </li>
-          ))}
-        </ul>
+      <div className="h-screen overflow-hidden bg-white flex items-center justify-center">
+        <div className="relative isolate px-6 py-14 lg:px-8 w-full h-full flex items-center justify-center flex-col">
+          <div
+              aria-hidden="true"
+              className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
+          >
+            <div
+                style={{
+                  clipPath:
+                      'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
+                }}
+                className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
+            />
+          </div>
+          <div className="mx-auto mt-10 mb-auto max-w-lg rounded-lg bg-white p-5 shadow-md">
+            <h2 className="text-lg text-black font-medium mb-5">Gallery Photos</h2>
+            {error && <p className="text-red-500">{error}</p>}
+            <ul className="space-y-3">
+              {files.map((file) => (
+                  <li key={file.cid}>
+                    <a
+                        href={file.url}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="text-blue-600 hover:underline"
+                    >
+                      View File (CID: {file.cid})
+                    </a>
+                  </li>
+              ))}
+            </ul>
+          </div>
+          <div
+              aria-hidden="true"
+              className="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]"
+          >
+            <div
+                style={{
+                  clipPath:
+                      'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
+                }}
+                className="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]"
+            />
+          </div>
+        </div>
       </div>
-    </div>
   );
 }
